bc62f38cf964adc1500a558ad4b87fe1381d9a2f
paul <paul2309825@gmail.com>
2022-06-11 23:32:14 +0800

    mixin

--- src/main/java/com/paul90317/dis_enchanting_book/MainMod.java
+++ src/main/java/com/paul90317/dis_enchanting_book/MainMod.java
@@ -1,17 +1,13 @@
 package com.paul90317.dis_enchanting_book;
 
+import com.paul90317.dis_enchanting_book.core.init.ModItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class MainMod implements ModInitializer {
-    // an instance of our new item
-    public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("dis_enchanting_book", "dis_enchanting_book"), CUSTOM_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("dis_enchanting_book", "dis_enchanting_book"), ModItems.DisEnchantingBook);
     }
 }
---
+++ src/main/java/com/paul90317/dis_enchanting_book/core/init/ModItems.java
@@ -0,0 +1,9 @@
+package com.paul90317.dis_enchanting_book.core.init;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class ModItems {
+    public static final Item DisEnchantingBook = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+}
